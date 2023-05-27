9bb769beeecdd7e83ebd4d22cce63d2a6dec71fd
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2023-05-27 08:15:32 +0700

    menambahkan fungsi login pada controller auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
-    //
+    public function loginAuth(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+    
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/')
+                        ->withSuccess('Signed in');
+        }
+   
+        return redirect('login')->withSuccess('Login details are not valid');
+    }     
+ 
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+   
+        return redirect('login');
+    }
 }
