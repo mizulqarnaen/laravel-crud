07bad0e89d7a32b3b5e406cf89b934594c1e6570
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2023-05-27 08:06:12 +0700

    membuat file migration untuk transaction dan customer

---
+++ database/migrations/2023_05_27_010503_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->string('payment_type');
+            $table->integer('shipping_cost');
+            $table->integer('total_amount');
+            $table->text('description')->nullable();
+            $table->string('source');
+            $table->timestamps();
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
