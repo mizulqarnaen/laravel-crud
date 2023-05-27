7007c71e567a067c3374c00926a8bdc8432ebd8e
Mohammad Iqbal <mohammad.iqbal.2409@gmail.com>
2023-05-27 08:10:41 +0700

    membuat file factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'customer_id' => $this->faker->numberBetween(1, 10),
+            'payment_type' => $this->faker->word(),
+            'shipping_cost' => $this->faker->randomNumber(5, true),
+            'total_amount' => $this->faker->randomNumber(6, true),
+            'description' => $this->faker->paragraph(),
+            'source' => $this->faker->word()
+        ];
+    }
+}
